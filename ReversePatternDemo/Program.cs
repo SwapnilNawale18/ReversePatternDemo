dfbfecb153d9f0265cd9a4775b9923549aed28a4
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 20:07:26 +0530

    Add project files.

---
+++ ReversePatternDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReversePatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = rows; i >=1; i--)
+            {
+                for (j = rows; j >= i; j--)
+                    Console.Write("{0} ", j);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
